7bfe1124e38407ffae5b1ae18822790d220bf5fe
Mio Veken <mio@cadout.de>
2024-09-10 15:14:50 +0200

    init

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+console.log('trigger polyfills',
+  Object.fromEntries([['one', 1]]),
+  Promise.withResolvers(),
+  'test'.includes('test'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+import Encore from '@symfony/webpack-encore';
+
+if(!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+  .enableSingleRuntimeChunk()
+  // .splitEntryChunks()
+
+  .setOutputPath('dist')
+  .setPublicPath('/')
+  .addEntry('main', './src/js/index.js')
+  // .setManifestKeyPrefix('')
+
+  .cleanupOutputBeforeBuild()
+
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = 'usage';
+    config.corejs = 3;
+    // config.targets.esmodules = true;
+    config.shippedProposals = true;
+    config.debug = true;
+  })
+;
+
+const config = Encore.getWebpackConfig();
+
+
+export default config;
+
